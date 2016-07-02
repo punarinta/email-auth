d043babba7d999eb5742f491c9a6700b73efe592
punarinta <cheaterx@yandex.ru>
2016-07-02 15:39:34 +0200

    SMTP discovery

--- src/Discover.php
+++ src/Discover.php
@@ -52,7 +52,58 @@ class Discover
         (
             'host'       => $host,
             'port'       => $port,
-            'encryption' =>  993 == $port ? 'ssl' : null,
+            'encryption' => 993 == $port ? 'ssl' : null,
+        ) : null;
+    }
+
+    /**
+     * Discovers SMTP settings for an email
+     *
+     * @param $email
+     * @return array|null
+     * @throws \Exception
+     */
+    public function smtp($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            throw new \Exception('Not a valid email');
+        }
+
+        $host = null;
+        $domain = explode('@', $email);
+
+        if ($port = Socket::pingPort('smtp.' . $domain[1], [465, 587, 25]))
+        {
+            $host = 'smtp.' . $domain[1];
+        }
+        elseif ($mxServer = Dns::getTopMx($domain[1]))
+        {
+            $mxServerDomains = explode('.', $mxServer);
+            $mxServerRoot = @implode('.', array_slice($mxServerDomains, -2, 2));
+
+            if ($port = Socket::pingPort($mxServer, [465, 587, 25]))
+            {
+                $host = $mxServer;
+            }
+            else if ($port = Socket::pingPort('smtp.' . $mxServerRoot, [465, 587, 25]))
+            {
+                $host = 'smtp.' . $mxServerRoot;
+            }
+        }
+
+        $encTypes =
+        [
+            25  => null,
+            465 => 'ssl',
+            587 => 'tls',
+        ];
+
+        return $host ? array
+        (
+            'host'       => $host,
+            'port'       => $port,
+            'encryption' => @$encTypes[$port] ?: null,
         ) : null;
     }
 }
